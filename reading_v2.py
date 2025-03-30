e0db342b8bdc6b7b73e1ffea1031e2247f76de2c
adria-boada <94541540+adria-boada@users.noreply.github.com>
2025-03-30 18:12:26 +0200

    Actualitza reading_v2.py
    He afegit una funció que arriba a crear un diccionari de dades segons el
    format de moments. Tot d'una tirada.
    
    Actualment, l'arxiu temporal amb la base de dades és massa gran i
    sqlite3 marca un "OperationalError: database or disk is full."
    La partició "/tmp" s'omple perquè l'arxiu amb la base de dades és de
    +19 Gb. S'hauria d'utilitzar un altre directori on manipular l'arxiu
    temporal; o fer-ho en el "current working directory"...

--- reading_v2.py
+++ reading_v2.py
@@ -15,7 +15,11 @@ Available formats for conversion: *sync or *ngsPool.out (output from ngsJulia).
 import sys
 # Reading gzipped files.
 import gzip
-# Assess file sizes.
+# Create sqlite databases as temporary files.
+import sqlite3
+from tempfile import NamedTemporaryFile
+# Sanitize sqlite input instead of using placeholders.
+import re
 import os
 
 
@@ -31,7 +35,7 @@ def generator_loci(finput: str, nchr: int, lrt_snp_thresh: float=6.64):
     Read a file with polymorphism data. Can be formatted as an "*ngsPool.out" or
     a "*sync" (possibly gzipped). Yields dictionaries with data per row.
 
-    Usage: 'for data in generator_loci(file, nchr): analyze data'
+    Usage: 'for row_data in generator_loci(file, nchr): process row_data'
 
     Input
     -----
@@ -202,13 +206,14 @@ def generator_loci(finput: str, nchr: int, lrt_snp_thresh: float=6.64):
 
 def generator_sites(finput: str, chr_column: int=0, pos_column: int=1):
     """
-    Read a BED-like file. Yield pairs of "chromosome" and "site" per row.
+    Read a BED file. Yield pairs of "chromosome" and "site" per row.
 
     Input
     -----
 
     finput : str
-    File input name.
+    File input name. Comments and headers within the BED file must start
+    with "#".
 
     chr_column : int
     The index (zero-based) of the column within "finput" with "chromosome" info.
@@ -232,8 +237,140 @@ def generator_sites(finput: str, chr_column: int=0, pos_column: int=1):
                         + f" '{pos_column}' and also if there are comments"
                         + " and headers which do not start with '#' as"
                         + " their first character.")
-            yield chromosome, pos
+            yield {
+                "chr": chromosome,
+                "pos": pos, }
 
+def sanitized_sqlite(parameter: str):
+    if not re.fullmatch( r"[a-zA-Z0-9_]+", parameter):
+        raise Exception(f"The parameter '{parameter}' is not"
+                        + " sanitized for it to be used with"
+                        + " sqlite3; it must be a string"
+                        + " matching '[a-zA-Z0-9_]+'.")
+
+def populate_new_sqlite_table(generator: iter, tblname: str, db_sqlite: str):
+    """
+    Input
+    -----
+
+    finput : str
+    File input name. Accepts BED, *ngsPool.out and *sync. Can be uncompressed or
+    gzipped.
+
+    tblname : str
+    Name of the newly created table within the sqlite database file.
+
+    db_sqlite : str
+    File name of the sqlite database which this function will write to.
+    """
+
+    # Open sqlite database.
+    con = sqlite3.connect(db_sqlite)
+    cur = con.cursor()
+    # Look at the fields/columns returned by "generator".
+    first_row = next(generator)
+    # Make sure they include required "chromosome" and "pos" fields.
+    if "chr" not in first_row.keys() or "pos" not in first_row.keys():
+        raise Exception("'Generator' from input file does not have"
+                        + " 'chr' or 'pos' fields.")
+    # Try to avoid SQL injections...
+    [sanitized_sqlite(param) for param in (tblname, *first_row.keys())]
+    # Create a table with the name "tblname" and the fields/columns in
+    # "first_row.keys()".
+    cur.execute(f"CREATE TABLE {tblname} ("
+                + ", ".join(first_row.keys()) + ", "
+                + " PRIMARY KEY(chr, pos) )")
+    # Add the data in "first_row" to the newly created table.
+    command_insert_sqlite = str(
+        f"INSERT INTO {tblname} VALUES ("
+        + ", ".join([":" + key for key in first_row.keys()]) + ")")
+    cur.execute(command_insert_sqlite, first_row)
+    # Add the data in the rest of the rows of the file input.
+    for row in generator:
+        cur.execute(command_insert_sqlite, row)
+    # Commit these INSERT changes.
+    con.commit()
+
+def build_moments_dd(finputs: list, pop_labels: list, nchroms: list,
+                      intersect_sites: str=None):
+    """
+    Build a 'moments.data_dictionary' from calling data.
+    """
+
+    # Check that the three lists in parameters are of the same length.
+    if len(finputs) != len(pop_labels) or len(finputs) != len(nchroms):
+        raise Exception("The lists of file inputs, pop. labels and num."
+                        + " of chromosomes must be of the same length.")
+    # Create a temporary file where an sqlite3 database will be stored.
+    temp_file = NamedTemporaryFile()
+    # Create a table within the database for each input file.
+    for fi, nc, pl in zip(finputs, nchroms, pop_labels):
+        generator = generator_loci(fi, nc)
+        populate_new_sqlite_table(generator, pl, temp_file.name)
+    # Add a table with sites from "intersect sites".
+    if intersect_sites:
+        generator = generator_sites(intersect_sites)
+        # Remove directory and extensions from filename so it can be used as
+        # table name.
+        intersect_tblname = os.path.split(intersect_sites)[1].split(".")[0]
+        populate_new_sqlite_table(generator, intersect_tblname,
+                                  temp_file.name)
+    # Select an inner join of all the tables, equivalent to sites shared across
+    # all files/tables.
+    con = sqlite3.connect(temp_file.name)
+    cur = con.cursor()
+    command_intersection_sqlite = str(
+        f"SELECT * FROM {pop_labels[0]} "
+        + " ".join(["INNER JOIN " + pl + " USING (chr,pos)"
+                    for pl in pop_labels[1:]]))
+    if intersect_sites:
+        command_intersection_sqlite += str(
+            f" INNER JOIN {intersect_tblname} USING (chr,pos)")
+    print("INFO: Executing the inner join (finding sites shared across"
+          + " all of the given files).")
+    res = cur.execute(command_intersection_sqlite)
+
+    # Initialize a dictionary which will be later read by moments and converted
+    # to an SFS.
+    moments_dd = dict()
+    print("INFO: Building a 'moments data dictionary'.")
+    # Iterate across each row (sites) of the inner joined tables.
+    for row in res:
+        site = tuple(row[0:2])
+        maj_alleles = tuple([
+            str(row[i]) for i in range(2, 8 * len(pop_labels) + 2, 8)])
+        min_alleles = tuple([
+            str(row[i]) for i in range(6, 8 * len(pop_labels) + 6, 8)])
+        # Obtain all of the observed alleles at this site.
+        alleles = list()
+        for a in maj_alleles + min_alleles:
+            if a not in alleles and a != "N": alleles.append(a)
+        alleles = tuple(alleles)
+        # If the site is triallelic, skip it.
+        if len(alleles) > 2: continue
+        if len(alleles) == 1:
+            alleles = (alleles[0], "N")
+        maj_counts = tuple([
+            str(row[i]) for i in range(5, 8 * len(pop_labels) + 5, 8)])
+        min_counts = tuple([
+            str(row[i]) for i in range(9, 8 * len(pop_labels) + 9, 8)])
+        # Compute calls for each population.
+        calls = {
+            pop_lab:
+            (maj_count, min_count) if maj_allele == alleles[0]
+            else (min_count, maj_count)
+            for pop_lab, maj_allele, maj_count, min_count in zip(
+                pop_labels, maj_alleles, maj_counts, min_counts)
+        }
+        # Store the information for this row in "moments_dd".
+        moments_dd[site] = {
+            "context": "---",
+            "outgroup_context": "---",
+            "outgroup_allele": "-",
+            "segregating": alleles,
+            "calls": calls, }
+
+    return moments_dd
 
 
 
